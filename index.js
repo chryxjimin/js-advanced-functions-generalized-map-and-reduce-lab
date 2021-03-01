b1500c33926ae57942eb7db64c10a35c78abcf01
Your Username <you@example.com>
2021-03-01 11:07:26 -0500

    commit tree 6ed3d38f794086e99d0f4031c976eb4d9508f189

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // Add your functions here
 function map(src, cb) {
     let newArray = []
-    src.map(function (cb) {
-        newArray.push(cb * -1)
-    })
+    let i;
+    for (i=0; i <src.length; i++) {
+        newArray.push(cb(src[i]))
+}
     return newArray
+}
+
+
+
+function reduce(src, cb, startingPoint){
+    let returnValue = (!!startingPoint) ? startingPoint : src[0];
+    let i = (!!startingPoint) ? 0 : 1
+    for (i; i < src.length; i++) {
+        returnValue = cb(src[i], returnValue)
+    }
+    return returnValue
 }
